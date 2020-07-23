b17871a83f626ffcf5293768e25492ba8005289b
SravanKumar35 <kumarsravan9182@gmail.com>
2020-07-23 18:21:09 +0530

    Updates

---
+++ precache-manifest.698b78502dd2930ff0474885745909d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd88874fa702038a0b5c32556620e429",
+    "url": "/React-list/index.html"
+  },
+  {
+    "revision": "10ebbd723ff6ed499ec9",
+    "url": "/React-list/static/css/main.87e04780.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/React-list/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-list/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ebbd723ff6ed499ec9",
+    "url": "/React-list/static/js/main.5126cfd6.chunk.js"
+  },
+  {
+    "revision": "ddffc57e3ddd60b9859d",
+    "url": "/React-list/static/js/runtime-main.ac5947c9.js"
+  }
+]);
